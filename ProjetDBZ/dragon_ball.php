6ad27570054ec8fa6de8241e9ba4f521c8dc5ccd
khaledze <khaledaliahmed397@gmail.com>
2023-10-26 11:27:20 +0200

    commit

--- ProjetDBZ/dragon_ball.php
+++ ProjetDBZ/dragon_ball.php
@@ -2,3 +2,18 @@
 class Personnage{
     
 }
+class heros extends Personnage{
+    private $nom;
+
+ public function __construct($nom,$vie,$niveau_puissabce)
+ parent::__construct($nom,$vie,$niveau_puissabce)
+ {
+        $this->nom=$nom;
+        $this->vie=$vie;
+        $this->niveau_puissabce=$niveau_puissabce;
+    }
+    
+}
+class vilains extends Personnage{
+    
+}
