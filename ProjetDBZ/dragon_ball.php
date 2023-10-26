0dbdf8e7f5086eb831d58437f06533294feee7e3
khaledze <khaledaliahmed397@gmail.com>
2023-10-26 12:42:13 +0200

    commit

--- ProjetDBZ/dragon_ball.php
+++ ProjetDBZ/dragon_ball.php
@@ -3,46 +3,18 @@ class Personnage{
     protected $nom;
     protected $niveau_puissance;
     protected $vies;
-    protected $attaque_special;
+    protected $attaque_speciale;
     protected $degats;
     protected $attaque_speciale_disponible;
 
     public function __construct($A,$N,$D,$V)
     {
-        $this->attaque_special=$A;
+        $this->attaque_speciale=$A;
         $this->nom = $N;
         $this->niveau_puissance=1;
         $this->degats=$D;
         $this->vies=$V;
         $this->attaque_speciale_disponible = true;
-    }
-
-    public function choixCamp(){
-        echo "vous voulez être dans quel camp";
-        $choix = strtolower(readline("1. Héros \n 2. Vilains"));
-        $heros = array();
-        $vilains = array();
-
-        switch ($choix) {
-            case '1':
-            case 'héros':
-                $heros[] = new Heros("Kamehameha", "Son Goku", "", 35, 300);
-                $heros[] = new Heros("Final Flash", "Vegeta","", 30,140);
-                $heros[] = new Heros("Special Beam Cannon", "Piccolo","",20, 130);
-                break;
-                
-            case '2':
-            case 'vilains':
-                $vilains[] = new Vilains("Death Ball", "Freezer", 40, 275);
-                $vilains[] = new Vilains("Solar Kamehameha", "Cell", 27, 180);
-                $vilains[] = new Vilains("Planet Burst", "Buu", 34, 160);
-                break;
-
-            default:
-                echo "Choix invalide.\n";
-                break;
-        }
-
     }
 
     public function attaque($cible) {
@@ -85,7 +57,7 @@ class Vilains extends Personnage{
     public function __construct($A,$N,$D,$V,$AV)
     {
         parent::__construct($A,$N,$D,$V);
-        $this->attaque_vilaine = $AV;
+        $this->attaque_speciale = $AV;
     }
 }
 
@@ -118,7 +90,7 @@ while (true) {
     }
 
     // Recharger l'attaque spéciale après deux rounds
-    if ($attaquant->attaque_speciale_disponible == false) {
+    if (!$attaquant->attaque_speciale_disponible) {
         $attaquant->recharge_attaque_speciale();
     }
 
