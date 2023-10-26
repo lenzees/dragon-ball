4479f38e97f8b64ef2107b36e8b8e82f15b9182d
lenzees <tcely2003@gmail.com>
2023-10-26 12:41:55 +0200

    class

--- ProjetDBZ/dragon_ball.php
+++ ProjetDBZ/dragon_ball.php
@@ -1,36 +1,34 @@
 <?php
-class Personnage{
+class Personnage {
     protected $nom;
     protected $niveau_puissance;
     protected $vies;
-    protected $attaque_special;
+    protected $attaque_speciale;
     protected $degats;
-    protected $attaque_speciale_disponible;
 
-    public function __construct($A,$N,$D,$V)
-    {
-        $this->attaque_special=$A;
+    public function __construct($A, $N, $D, $V) {
+        $this->attaque_speciale = $A;
         $this->nom = $N;
-        $this->niveau_puissance=1;
-        $this->degats=$D;
-        $this->vies=$V;
-        $this->attaque_speciale_disponible = true;
+        $this->niveau_puissance = 1;
+        $this->degats = $D;
+        $this->vies = $V;
     }
 
-    public function choixCamp(){
-        echo "vous voulez être dans quel camp";
-        $choix = strtolower(readline("1. Héros \n 2. Vilains"));
+    public function choixCamp() {
+        echo "Vous voulez être dans quel camp ?\n";
+        $choix = strtolower(readline("1. Héros \n2. Vilains\n"));
+        
         $heros = array();
         $vilains = array();
 
         switch ($choix) {
             case '1':
             case 'héros':
-                $heros[] = new Heros("Kamehameha", "Son Goku", "", 35, 300);
-                $heros[] = new Heros("Final Flash", "Vegeta","", 30,140);
-                $heros[] = new Heros("Special Beam Cannon", "Piccolo","",20, 130);
+                $heros[] = new Heros("Kamehameha", "Son Goku", 35, 300);
+                $heros[] = new Heros("Final Flash", "Vegeta", 30, 140);
+                $heros[] = new Heros("Special Beam Cannon", "Piccolo", 20, 130);
                 break;
-                
+
             case '2':
             case 'vilains':
                 $vilains[] = new Vilains("Death Ball", "Freezer", 40, 275);
@@ -42,89 +40,17 @@ class Personnage{
                 echo "Choix invalide.\n";
                 break;
         }
-
-    }
-
-    public function attaque($cible) {
-        // Attaquer la cible
-        echo $this->nom." attaque ".$cible->nom." !\n";
-        $degats = $this->niveau_puissance * $this->degats;
-        $cible->vies -= $degats;
-    }
-
-    public function attaque_speciale($cible) {
-        // Vérifier si l'attaque spéciale est disponible
-        if (!$this->attaque_speciale_disponible) {
-            echo "L'attaque spéciale n'est pas disponible pour le moment.\n";
-            return;
-        }
-
-        // Utiliser l'attaque spéciale
-        echo $this->nom." utilise son attaque spéciale sur ".$cible->nom." !\n";
-        $degats = $this->niveau_puissance * 30;
-        $cible->vies -= $degats;
-        $this->attaque_speciale_disponible = false;
-    }
-
-    public function recharge_attaque_speciale() {
-        // Recharger l'attaque spéciale
-        $this->attaque_speciale_disponible = true;
-        echo $this->nom." a rechargé son attaque spéciale !\n";
     }
 }
 
-class Heros extends Personnage{
-    public function __construct($A,$N,$D,$V,$AS)
-    {
-        parent::__construct($A,$N,$D,$V);
-        $this->attaque_speciale = $AS;
+class Heros extends Personnage {
+    public function __construct($attaque_speciale, $nom, $degats, $vies) {
+        parent::__construct($attaque_speciale, $nom, $degats, $vies);
     }
 }
 
-class Vilains extends Personnage{
-    public function __construct($A,$N,$D,$V,$AV)
-    {
-        parent::__construct($A,$N,$D,$V);
-        $this->attaque_vilaine = $AV;
-    }
-}
-
-// Créer des héros et des vilains
-$goku = new Heros("Kamehameha", "Son Goku", 35, 300, "Super Kamehameha");
-$vegeta = new Heros("Final Flash", "Vegeta", 30, 140, "Big Bang Attack");
-$freezer = new Vilains("Death Ball", "Freezer", 40, 275, "Supernova");
-$cell = new Vilains("Solar Kamehameha", "Cell", 27, 180, "Perfect Barrier");
-$heros = array($goku, $vegeta);
-$vilains = array($freezer, $cell);
-
-// Boucle de combat
-while (true) {
-    // Sélectionner un héros et un vilain aléatoirement
-    $attaquant = $heros[array_rand($heros)];
-    $cible = $vilains[array_rand($vilains)];
-
-    // Attaquer la cible
-    $attaquant->attaque($cible);
-
-    // Vérifier si la cible est morte
-    if ($cible->vies <= 0) {
-        echo $cible->nom." est mort !\n";
-        break;
-    }
-
-    // Attaquer avec l'attaque spéciale si elle est disponible
-    if ($attaquant->attaque_speciale_disponible) {
-        $attaquant->attaque_speciale($cible);
-    }
-
-    // Recharger l'attaque spéciale après deux rounds
-    if ($attaquant->attaque_speciale_disponible == false) {
-        $attaquant->recharge_attaque_speciale();
-    }
-
-    // Vérifier si l'attaquant est mort
-    if ($attaquant->vies <= 0) {
-        echo $attaquant->nom." est mort !\n";
-        break;
+class Vilains extends Personnage {
+    public function __construct($attaque_speciale, $nom, $degats, $vies) {
+        parent::__construct($attaque_speciale, $nom, $degats, $vies);
     }
 }
