1362fd53ff1ed91abe6528d8ee9d1da19b2c91a9
lenzees <tcely2003@gmail.com>
2023-10-26 11:26:46 +0200

    personnage

--- ProjetDBZ/dragon_ball.php
+++ ProjetDBZ/dragon_ball.php
@@ -1,4 +1,13 @@
 <?php
 class Personnage{
-    
+    protected $nom;
+    protected $niveau_puissance;
+    protected $vies;
+
+    public function __construct($N,$Nv,$V)
+    {
+        $this->nom=$N;
+        $this->niveau_puissance=$Nv;
+        $this->vies=$V;
+    }
 }
